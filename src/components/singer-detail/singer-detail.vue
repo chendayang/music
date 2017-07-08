019854ed6e5d6613507bb26dfc3c44934591eafa
chenbingyang <chenbingyang@gracepsy.com>
2017-07-08 17:17:40 +0800

    6-14 完结

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,10 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-    </div>
+    <music-list
+    :songs="songs"
+    :title="title"
+    :bg-image="bgImage"
+    ></music-list>
   </transition>
 </template>
 
@@ -10,19 +13,29 @@ import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 export default {
   data() {
     return {
-      song: []
+      songs: []
     }
   },
   computed: {
     ...mapGetters([
       'singer'
-    ])
+    ]),
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    }
   },
   created() {
     this._getDetail()
+  },
+  components: {
+    MusicList
   },
   methods: {
     _getDetail() {
@@ -32,7 +45,7 @@ export default {
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
           this.songs = this._normalizeSongs(res.data.list)
-          console.log(this.songs, 'this.songs')
+          // console.log(this.songs, 'this.songs')
         }
       })
     },
@@ -53,15 +66,7 @@ export default {
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
 
-  .singer-detail
-    position fixed
-    z-index 100
-    top 0
-    left 0
-    right 0
-    bottom 0
-    background $color-background
-  .slide-enter-acitve, .slide-leave-active
+  .slide-enter-active,.slide-leave-active
     transition all 0.3s
   .slide-enter,.slide-leave-to
     transform:translate3d(100%,0,0)
