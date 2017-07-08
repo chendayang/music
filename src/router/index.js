bab4c821f230bdaa36e1a20b94a03bbf0f77cb14
chenbingyang <chenbingyang@gracepsy.com>
2017-07-08 10:50:18 +0800

    6.2 子路由和动画

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Recommend from 'components/recommend/recommend'
 import Rank from 'components/rank/rank'
 import Singer from 'components/singer/singer'
 import Search from 'components/search/search'
+import SingerDetail from 'components/singer-detail/singer-detail'
 Vue.use(Router)
 
 export default new Router({
@@ -25,7 +26,13 @@ export default new Router({
     {
       path: '/singer',
       component: Singer,
-      name: 'Singer'
+      name: 'Singer',
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
     },
     {
       path: '/search',
