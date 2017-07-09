16c4653423c59be1431370049b96f84fa1c85413
chenbingyang <chenbingyang@gracepsy.com>
2017-07-09 11:49:09 +0800

    7-3 vuex的action

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="song-list">
     <ul>
-      <li v-for="song in songs" class="item">
+      <li
+      @click="selectItem(song, index)"
+      v-for="(song, index) in songs"
+      class="item">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -23,6 +26,9 @@
     methods: {
       getDesc(song) {
         return `${song.singer}.${song.album}`
+      },
+      selectItem(item, index) {
+        this.$emit('select', item, index)
       }
     }
   }
